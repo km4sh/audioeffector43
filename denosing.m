61615b9f1afff1029dcd6a314888ef67c76041d8
Matt Ma <mattma9209@gmail.com>
2016-10-28 01:45:27 +0800

    Delete denosing.m

--- denosing.m
+++
@@ -1,7 +0,0 @@
-function denoisingfilename = denosing(in)
-[wav,fs] = audioread(in);
-wavL = wav(1:length(wav));
-wavR = wav(length(wav)+1:2*length(wav));
-out = wavL-wavR;
-audiowrite(['denoising_',in],out,fs);
-denoisingfilename = ['denoising_',in];
